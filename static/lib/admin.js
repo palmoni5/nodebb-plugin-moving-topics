ca6f6784c1b9c314c28354dc5d02e7107ac91b0d
palmoni5 <ff8401948@gmail.com>
2026-03-15 20:50:10 +0200

    Fix admin setting load and add move limit message

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -3,14 +3,12 @@
 /* global ajaxify, app */
 
 let inserted = false;
-let prepared = false;
 
 $(window).on('action:app.load', function () {
 	require(['hooks', 'translator', 'admin/settings'], function (hooks, translator, Settings) {
 		hooks.on('action:ajaxify.end', function () {
 			if (!ajaxify.data || !ajaxify.data.template || !ajaxify.data.template['admin/settings/post']) {
 				inserted = false;
-				prepared = false;
 				return;
 			}
 
@@ -19,35 +17,34 @@ $(window).on('action:app.load', function () {
 				return;
 			}
 
-			if (!inserted) {
-				const sectionHtml = [
-					'<hr/>',
-					'<div id="moving-topics-settings" class="mb-4">',
-					'<h5 class="fw-bold tracking-tight settings-header">[[moving-topics:admin.settings.title]]</h5>',
-					'<div class="mb-3">',
-					'<label class="form-label" for="movingTopicsMaxPosts">[[moving-topics:admin.settings.max-posts]]</label>',
-					'<input id="movingTopicsMaxPosts" type="number" min="0" class="form-control" data-field="movingTopicsMaxPosts" placeholder="5">',
-					'<p class="form-text">[[moving-topics:admin.settings.max-posts-help]]</p>',
-					'</div>',
-					'</div>',
-				].join('');
+			if (inserted) {
+				return;
+			}
 
-				translator.translate(sectionHtml, function (translated) {
-					container.append(translated);
-					if (!app.config.hasOwnProperty('movingTopicsMaxPosts')) {
-						$('#movingTopicsMaxPosts').val(5);
-					} else {
-						$('#movingTopicsMaxPosts').val(app.config.movingTopicsMaxPosts);
-					}
-				});
+			const sectionHtml = [
+				'<hr/>',
+				'<div id="moving-topics-settings" class="mb-4">',
+				'<h5 class="fw-bold tracking-tight settings-header">[[moving-topics:admin.settings.title]]</h5>',
+				'<div class="mb-3">',
+				'<label class="form-label" for="movingTopicsMaxPosts">[[moving-topics:admin.settings.max-posts]]</label>',
+				'<input id="movingTopicsMaxPosts" type="number" min="0" class="form-control" data-field="movingTopicsMaxPosts" placeholder="5">',
+				'<p class="form-text">[[moving-topics:admin.settings.max-posts-help]]</p>',
+				'</div>',
+				'</div>',
+			].join('');
 
+			translator.translate(sectionHtml, function (translated) {
+				container.append(translated);
 				inserted = true;
-			}
 
-			if (!prepared) {
-				prepared = true;
+				if (!app.config.hasOwnProperty('movingTopicsMaxPosts')) {
+					$('#movingTopicsMaxPosts').val(5);
+				} else {
+					$('#movingTopicsMaxPosts').val(app.config.movingTopicsMaxPosts);
+				}
+
 				Settings.prepare();
-			}
+			});
 		});
 	});
 });
